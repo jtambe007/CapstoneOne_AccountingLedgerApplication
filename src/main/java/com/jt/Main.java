a0fb50703a0325506e9cb4c25e2f9e1a360b8101
jtambe007 <marietambe07@gmail.com>
2023-05-02 12:33:42 -0400

    updated addDeposit() method

--- src/main/java/com/jt/Main.java
+++ src/main/java/com/jt/Main.java
@@ -1,9 +1,6 @@
 package com.jt;
-import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
@@ -32,24 +29,24 @@ public class Main {
 
             switch (commandInput) {
                 case "D":
-                    addDeposit();
+                    addDeposit(scanner);
                     break;
-                case "P":
-                    makePayment();
-                    break;
-//                case "L":
-//                    displayLedger();
+//                case "P":
+//                    makePayment(scanner);
 //                    break;
-//                case "X":
-//                    System.out.println("Exiting the application...");
-//                    break;
+////                case "L":
+////                    displayLedger();
+////                    break;
+////                case "X":
+////                    System.out.println("Exiting the application...");
+////                    break;
                 default:
                     System.out.println("Sorry, the command you entered was not found. Please try again.");
             }
         } while (!commandInput.equals("X"));
     }
 
-    public static void addDeposit(Scanner scanner){
+    public static void addDeposit(Scanner scanner) {
         System.out.println("Please enter the date (yyyy-MM-dd) of the transaction");
         String depositDate = scanner.nextLine();
         System.out.println("Please enter the time (hh:mm:ss) of the transaction");
@@ -59,40 +56,42 @@ public class Main {
         System.out.println("Please enter the name of the vendor for the transaction");
         String depositVendor = scanner.nextLine();
         System.out.println("Please enter the deposit amount for the transaction");
-        String depositAmount = scanner.nextLine();
+        double depositAmount = Double.parseDouble(scanner.nextLine());
 
         try {
             FileWriter writer = new FileWriter("transactions.txt", true);
-            writer.append(depositDate + "|" + depositTime + "|" + depositDescription + "|" + depositVendor +
+            writer.append("\n" + depositDate + "|" + depositTime + "|" + depositDescription + "|" + depositVendor +
                     "|" + depositAmount);
             System.out.println("Deposit added successfully");
+            writer.close();
         } catch (IOException e) {
             System.out.println("An error occurred while adding this deposit");
         }
-    }
 
-    private static void makePayment(Scanner scanner) {
-        System.out.println("Please enter the description of the transaction you want to pay for");
-        String userDescription = scanner.nextLine();
-
-    }
-
-    private static void displayLedger() {
-        // Read transaction information from CSV file and display it on the screen
-        System.out.println("Transaction Ledger:");
-        Scanner scanner = new Scanner("transactions.txt")
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] parts = line.split("\\|");
-                String date = parts[0];
-                String time = parts[1];
-                String description = parts[2];
-                String vendor = parts[3];
-                String amount = parts[4];
-                System.out.println("\This transaction entered on: " + date + " at " + time + ". It was for " + description.toLowerCase() + " from " + vendor + ". Amount: " + amount);
-            }
-        }
     }
 }
 
-
+//    private static void makePayment(Scanner scanner) {
+//        System.out.println("Please enter the description of the transaction you want to pay for");
+//        String userDescription = scanner.nextLine();
+//
+//    }
+//
+//    private static void displayLedger() {
+//        // Read transaction information from CSV file and display it on the screen
+//        System.out.println("Transaction Ledger:");
+//        Scanner scanner = new Scanner("transactions.txt");
+//            while (scanner.hasNextLine()) {
+//                String line = scanner.nextLine();
+//                String[] parts = line.split("\\|");
+//                String date = parts[0];
+//                String time = parts[1];
+//                String description = parts[2];
+//                String vendor = parts[3];
+//                String amount = parts[4];
+//                System.out.println("This transaction entered on: " + date + " at " + time + ". It was for " + description.toLowerCase() + " from " + vendor + ". Amount: " + amount);
+//            }
+//        }
+//    }
+//
+//
